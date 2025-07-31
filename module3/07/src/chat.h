921e37da0d08ce3fd889f902182c4a951905947e
Niknet19 <nikitos.feo12@example.com>
2025-07-31 22:50:43 +0700

    Сделал 07

---
+++ module3/07/src/chat.h
@@ -0,0 +1,24 @@
+#ifndef CHAT
+#define CHAT
+#include <fcntl.h>
+#include <mqueue.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define MSG_SIZE 128
+#define MAX_QUEUE_SIZE 10
+#define MSG_TYPE_PING 1
+#define MSG_TYPE_PONG 2
+#define QUEUE_NAME "/chat_queue"
+#define TRUE 1
+#define FALSE 0
+
+void handle_error(char* str) {
+  perror(str);
+  exit(EXIT_FAILURE);
+}
+
+#endif
